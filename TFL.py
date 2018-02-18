76f87045da7688caac027f13e5060f41444eec1a
Enrico <enrico.zammitl@gmail.com>
2018-02-18 06:22:17 +0000

    fix

--- TFL.py
+++ TFL.py
@@ -2,13 +2,9 @@ import requests
 import json
 
 def getRoute(origin,destination):
-<<<<<<< HEAD:TFL/TFL.py
-    origin=origin" Underground Station"
+    origin=origin+" Underground Station"
     destination=destination+" Underground Station"
-=======
-
     print(origin, destination)
->>>>>>> bb5d6f84f8390fd4a12b1573f5123facdc1088a2:TFL.py
     KEY=""
     fp=open('keys.txt', 'r')
     for line in fp:
@@ -33,4 +29,4 @@ def getRoute(origin,destination):
         fp.write(json.dumps(jasonData, indent=4, sort_keys=True))
     return totalData
 
-
+print(getRoute("Victoria","Baker Street"))
