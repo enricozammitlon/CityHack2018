bad1839707e5c393a93b51e248280ebd71e43ac6
tobyjamez <tobyswjames@gmail.com>
2018-02-18 00:17:22 +0000

    OAuth 2.0 authentication with spotify enabled

--- Spotify/spotifyQuerier.py
+++ Spotify/spotifyQuerier.py
@@ -3,19 +3,40 @@ import json
 
 
 def authenticate():
-	auth_url = 'https://accounts.spotify.com/authorize'
-params = dict(
-	'client_id' = '18d5540610164219adcaede028711873',
-	'redirect_uri' = 'http://tobyjames.xyz'
-	)
+	auth_url = 'https://accounts.spotify.com/api/token'
+
+	with open('auth.txt', 'r') as f:
+		content = f.readlines()
+		content = [x.strip() for x in content] 
+		refresh_code = content[0]
+		clientid = content[1]
+		clientsecret = content[2]	
+
+	headers = {'Content-Type':'application/x-www-form-urlencoded'}
+	params = dict(
+		grant_type = 'refresh_token',
+		refresh_token = refresh_code,
+		redirect_uri = 'http://tobyjames.xyz',
+		client_id = clientid,
+		client_secret = clientsecret
+		)
+	f.close()
+	response = requests.post(url = auth_url, params = params, headers = headers)
+	if 'refresh_token' in response.json():
+		f = open('auth.txt', 'w')
+		content[0] = response.json()['refresh_token']
+		f.writelines(content)
+	print(response.status_code)
+	return response.json()['access_token']
+
+	
 	
 
-	return auth_code
 
 
 def search(track_list):
 	
-	auth_code = authorise()
+	auth_code = authenticate()
 
 	headers = {'Authorization' : auth_code}
 	
@@ -24,10 +45,11 @@ def search(track_list):
 	for track in track_list:
 	
 		params = dict(
-			'q' = str(track).replace(" ", "%20"),
-			'type' = "track"i,
-			'limit' = '5')
+			q = str(track).replace(" ", "%20"),
+			type = "track",
+			limit = '5')
 
 		response = requests.get(url = url, headers = headers, params = params)
-
-
+	return response
+#authenticate()
+print(authenticate())
