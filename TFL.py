793c0b0d30e3a591b45f3db1ffc2b9dbe1d9f711
Enrico <enrico.zammitl@gmail.com>
2018-02-18 00:20:35 +0000

    Input origin and destination Output dictionary with time and stations.

---
+++ TFL.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+def getRoute(origin,destination):
+    KEY=""
+    fp=open('keys.txt', 'r')
+    for line in fp:
+        KEY=str(line)
+    print(KEY)
+    request="https://api.tfl.gov.uk/Journey/JourneyResults/"+origin+"/to/"+destination+str(KEY)
+    requestedData = requests.get(request)
+    jasonData = requestedData.json()
+    journeysDict=[origin]
+    counter=0
+    time=0
+
+    time=jasonData['journeys'][0]['duration']
+    for a in  jasonData['journeys']:
+        for b in a['legs']:
+            for c in b['path']['stopPoints']:
+                journeysDict.append(c['name'])
+                counter+=1
+    journeysDict=sorted(set(journeysDict))
+
+    totalData={"Time":time,"Stations":journeysDict}
+    with open('out.json', 'w') as fp:
+        fp.write(json.dumps(jasonData, indent=4, sort_keys=True))
+    return totalData
+
+origin=input("From:\n")+" Underground Station"
+destination=input("To:\n")+" Underground Station"
+
+print(origin)
+print(getRoute(origin,destination))
